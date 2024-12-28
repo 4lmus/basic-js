a2e9b7ad05da6f5ac7b669c6ce4444b53f171709
unknown <sashka.musatov@gmail.com>
2024-12-28 15:14:00 +0300

    feat: add functionality to count-cats.js and delete-digit.js files

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let nAsString = String(n);
+  let maxNumber = +(nAsString.slice(1));
+  let number;
+  for (let i = 0; i < nAsString.length; i += 1) {
+      let str2 = nAsString;
+      if(i === 0) {
+          number = +(str2.slice(1));
+      }
+      if(i === str2.length-1) {
+          number = +(str2.slice(0, str2.length-1));
+      }
+      else {
+          number = +(str2.slice(0, i) + str2.slice(i + 1));
+      }
+      if(number > maxNumber) {
+          maxNumber = number;
+      }
+  }
+  return maxNumber;
 }
 
 module.exports = {
