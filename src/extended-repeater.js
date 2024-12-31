8b5e111271366d083b6a15aac1474626e42ec1bd
unknown <sashka.musatov@gmail.com>
2024-12-31 09:19:23 +0300

    feat: add funcionality to extended-repeater.js and file-names.js files

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,56 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr = '';
+
+  if(options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+
+  if(options.additionRepeatTimes === undefined && options.addition !== undefined) {
+    options.additionRepeatTimes = 1;
+  }
+
+  if(options.separator === undefined) {
+      options.separator = '+';
+  }
+
+  if(options.additionSeparator === undefined) {
+      options.additionSeparator = '|';
+  }
+
+  if(typeof str !== 'string' || typeof options.addition !== 'string') {
+      str = String(str);
+      options.addition = String(options.addition);
+  }
+
+  for(let i = 0; i < options.repeatTimes; i += 1) {
+      newStr += str;
+      for(let k = 0; k < options.additionRepeatTimes; k += 1) {
+          newStr += options.addition;
+          if(k !== options.additionRepeatTimes - 1) {
+              newStr += options.additionSeparator;
+          }
+      }
+      if(i !== options.repeatTimes - 1) {
+          newStr += options.separator;
+      }
+  }
+  return newStr;
 }
 
 module.exports = {
--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  if(names.length === 0) {
+    return []
+  }
+  let newNames = [];
+  newNames.push(names[0]);
+  for(let i = 1; i < names.length; i += 1) {
+    let countRepeatedFiles = 0;
+    for(let k = 0; k < i; k += 1) {
+      if(names[k] === names[i]) {
+        countRepeatedFiles += 1;
+      }
+    }
+    if(countRepeatedFiles !== 0) {
+      newNames.push(names[i]+`(${countRepeatedFiles})`);
+    }
+    else {
+      newNames.push(names[i]);
+      for(let k = 0; k < i; k += 1) {
+        if(newNames[k] === newNames[i]) {
+            countRepeatedFiles += 1;
+        }
+      }
+      if(countRepeatedFiles !== 0) {
+        newNames = newNames.slice(0, -1);
+        newNames.push(names[i]+`(${countRepeatedFiles})`);
+      }
+    }
+  }
+  return newNames;
 }
 
 module.exports = {
